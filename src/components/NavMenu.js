f87ecce538d19a7e32fd89d6046c44d8bd775ee0
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-09-22 12:41:19 -0300

    Add NavMenu file

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,5 +1,16 @@
+import { Link } from 'react-router-dom';
+
 const NavMenu = () => (
-  <h1>Nav Menu</h1>
+  <header className="w-2/12 hidden md:flex flex-col border-solid border-2 border-gray">
+    <h1 className="self-center font-bold text-4xl pt-10">Vets App</h1>
+    <nav className="flex flex-col pl-3 mt-28">
+      <Link className="font-bold text-gray-800 hover:bg-citrus-500 hover:text-white text-lg pl-2 tracking-tighter py-2" href="/doctors">VETS</Link>
+      <Link className="font-bold text-gray-800 hover:bg-citrus-500 hover:text-white text-lg pl-2 tracking-tighter py-2" href="/">MAKE AN APPOINTMENT</Link>
+      <Link className="font-bold text-gray-800 hover:bg-citrus-500 hover:text-white text-lg pl-2 tracking-tighter py-2" href="/">MY APPOINTMENTS</Link>
+      <Link className="font-bold text-gray-800 hover:bg-citrus-500 hover:text-white text-lg pl-2 tracking-tighter py-2" href="/">ADD VET</Link>
+      <Link className="font-bold text-gray-800 hover:bg-citrus-500 hover:text-white text-lg pl-2 tracking-tighter py-2" href="/">DELETE VET</Link>
+    </nav>
+  </header>
 );
 
 export default NavMenu;
