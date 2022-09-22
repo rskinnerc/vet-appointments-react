9bf54555c0a789cbafb5d2b684447eb4f90a655a
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-09-22 12:40:54 -0300

    Add doctor reducer to store

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import auth from './authSlice';
+import doctor from './doctorSlice';
 
 const store = configureStore({
   reducer: {
     auth,
+    doctor,
   },
 });
 
