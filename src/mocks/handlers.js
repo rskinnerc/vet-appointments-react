9f512d811c685607465266386bb0d9c84cd5dc8b
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-09-24 14:18:27 -0300

    Add doctor and appointments mock data

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -9,6 +9,47 @@ const handlers = [
     }),
   )),
 
+  rest.get(`${process.env.REACT_APP_API_HOST}/doctors/index`, (req, res, ctx) => res(
+    ctx.json([
+      {
+        id: 1,
+        name: 'John Doe',
+        experience: 5,
+        specialization: 'Ophtalmology',
+        price: 50,
+        description: 'John description text',
+      },
+      {
+        id: 2,
+        name: 'Jane Doe',
+        experience: 10,
+        specialization: 'Cardiology',
+        price: 100,
+        description: 'Jane description text',
+      },
+    ]),
+  )),
+
+  rest.get(`${process.env.REACT_APP_API_HOST}/appointments/create`, (req, res, ctx) => res(
+    ctx.json([
+      {
+        id: 1,
+        doctor_id: 1,
+        user_id: 1,
+        city: 'California',
+        date: 50,
+      },
+
+      {
+        id: 2,
+        doctor_id: 2,
+        user_id: 2,
+        city: 'Prague',
+        date: 50,
+      },
+    ]),
+  )),
+
 ];
 
 export default handlers;
