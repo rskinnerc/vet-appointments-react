505b8a37a5e6028482982d6faebff3339f61afa8
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-09-26 16:56:19 -0300

    Add icons to mobile menu

--- src/components/MobileMenu.js
+++ src/components/MobileMenu.js
@@ -2,6 +2,7 @@
 import { NavLink } from 'react-router-dom';
 import useCollapse from 'react-collapsed';
 import { useState } from 'react';
+import { IoMenu, IoClose } from 'react-icons/io5';
 
 const MobileMenu = () => {
   const [isExpanded, setExpanded] = useState(false);
@@ -13,8 +14,12 @@ const MobileMenu = () => {
 
   return (
     <header className="lg:hidden flex flex-col border-solid border-2 border-gray">
-      <button type="button" className="absolute right-3" {...getToggleProps({ onClick: toggleCollapse })}>{isExpanded ? 'Close' : 'Menu'}</button>
-      <nav {...getCollapseProps()} className="top-8 absolute left-3 md:left-1/2 right-3 bg-gray-100/95 z-10 rounded-l">
+      <div className="absolute right-2 top-2" {...getToggleProps({ onClick: toggleCollapse })}>
+        {isExpanded
+          ? <IoClose className="text-3xl relative" />
+          : <IoMenu className="text-3xl hover:text-citrus-600 relative" />}
+      </div>
+      <nav {...getCollapseProps()} className="top-10 absolute left-3 md:left-1/2 right-3 bg-gray-100/95 z-10 rounded-l">
         <NavLink onClick={() => toggleCollapse()} className="font-bold text-gray-800 hover:bg-citrus-500 hover:text-white text-lg pl-2 tracking-tighter py-2 block rounded-tl" to="/doctors">VETS</NavLink>
         <NavLink onClick={() => toggleCollapse()} className="font-bold text-gray-800 hover:bg-citrus-500 hover:text-white text-lg pl-2 tracking-tighter py-2 block" to="/new-appointment">MAKE AN APPOINTMENT</NavLink>
         <NavLink onClick={() => toggleCollapse()} className="font-bold text-gray-800 hover:bg-citrus-500 hover:text-white text-lg pl-2 tracking-tighter py-2 block" to="/appointments">MY APPOINTMENTS</NavLink>
