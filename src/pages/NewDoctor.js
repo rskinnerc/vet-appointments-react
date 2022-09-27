674f232dbe3fda30b8f62da832f6766d3c795050
Ronald Skinner <rjskinnerc@unal.edu.co>
2022-09-27 10:34:52 -0500

    Add responsiveness to the new doctor page

--- src/pages/NewDoctor.js
+++ src/pages/NewDoctor.js
@@ -13,7 +13,7 @@ const NewDoctor = () => {
   const [experience, setExperience] = useState('');
   const [price, setPrice] = useState('');
   const [description, setDescription] = useState('');
-  const [photo, setPhoto] = useState('');
+  const [photo, setPhoto] = useState();
 
   useEffect(() => {
     if (addDoctorStatus === 'Successfully created') {
@@ -28,7 +28,9 @@ const NewDoctor = () => {
     formdata.append('doctor[specialization]', specialization);
     formdata.append('doctor[experience]', experience);
     formdata.append('doctor[price]', price);
-    formdata.append('doctor[photo]', photo);
+    if (photo) {
+      formdata.append('doctor[photo]', photo);
+    }
     formdata.append('doctor[description]', description);
 
     dispatch(addDoctor(formdata));
@@ -41,7 +43,7 @@ const NewDoctor = () => {
   return (
     <section className="w-full">
       <h1 className="text-3xl font-semibold text-center my-12">Add a Doctor</h1>
-      <form onSubmit={submit} className="flex flex-col gap-2 px-1">
+      <form onSubmit={submit} className="flex flex-col gap-2 px-1 md:w-4/6 lg:w-1/2 2xl:w-1/3 mx-auto">
         <input className="h-12" type="text" id="name" name="name" placeholder="Name" onChange={(e) => setName(e.target.value)} />
         <input className="h-12" type="text" id="specialization" name="specialization" placeholder="Specialization" onChange={(e) => setSpecialization(e.target.value)} />
         <input className="h-12" type="number" id="yearsOfExperience" name="experience" placeholder="Years of experience" onChange={(e) => setExperience(e.target.value)} />
@@ -49,6 +51,7 @@ const NewDoctor = () => {
         <textarea className="h-24" id="description" name="description" placeholder="Description" onChange={(e) => setDescription(e.target.value)} />
         <label htmlFor="doctor-photo" className="p-2">
           Doctor Photo
+          {' '}
           <input type="file" id="doctor-photo" name="doctor-photo" onChange={(e) => setPhoto(e.target.files[0])} />
         </label>
         <button type="submit" className="mx-auto h-12 flex justify-between items-center bg-citrus-500 hover:bg-citrus-600 hover:shadow-md text-white px-6 shadow-lg text-center rounded-full font-semibold">Add Doctor</button>
